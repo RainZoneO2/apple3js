95b3c7df9b1a5e856ff579aff736f3b906ad0b48
Ghaith A <alghaith.ah38@gmail.com>
2024-08-08 10:38:28 -0400

    Refactored into skeleton structure.

---
+++ src/Experience/World/World.js
@@ -0,0 +1,23 @@
+import Experience from "../Experience"
+import Environment from './Environment'
+
+export default class World {
+    constructor() {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+        // Wait for resources
+        this.resources.on('ready', () => {
+            // Setup
+            this.environment = new Environment()
+            this.loaded = true
+        })
+    }
+
+    update() {
+        if (this.loaded) {
+            // Update here
+        }
+    }
+}
---
+++ src/Experience/sources.js
@@ -0,0 +1,7 @@
+export default [
+    // {
+    //     name: 'appleModel',
+    //     type: 'gltfModel',
+    //     path: 'models/Apple.glb'
+    // }
+]
